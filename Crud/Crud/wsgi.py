5aed6e4670f17bdbd61e719b3d7e865cb01abb37
BastianSandovalR <ataymayo@gmail.com>
2025-02-11 16:21:11 -0300

    Update wsgi.py

--- Crud/Crud/wsgi.py
+++ Crud/Crud/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Crud.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Crud.Crud.settings')
 
 application = get_wsgi_application()
